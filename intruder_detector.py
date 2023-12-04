6e16fb6816ed770c02dec8222fdc950abe29da1b
VEGALITTLE\Rusll <rusllann97@gmail.com>
2023-12-04 23:37:08 +0500

    Добавлен код-пример с распознаванием движения.

--- intruder_detector.py
+++ intruder_detector.py
@@ -1,3 +1,51 @@
 import cv2
 
-idcap = cv2.VideoCapture('celebs.mp4')
+FILE_NAME = "video1.mp4"
+
+vidCap = cv2.VideoCapture(f'Video/{FILE_NAME}')
+
+vidCap.set(3,1280)
+vidCap.set(4,700)
+
+
+ret, frame1 = vidCap.read()
+ret, frame2 = vidCap.read()
+
+while vidCap.isOpened():
+    diff = cv2.absdiff(frame1, frame2)
+
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)  # фильтрация лишних контуров
+
+    _, thresh = cv2.threshold(blur, 100, 255, cv2.THRESH_BINARY) # метод для выделения кромки объекта белым цветом
+ 
+    dilated = cv2.dilate(thresh, None, iterations = 3) # данный метод противоположен методу erosion(), т.е. эрозии объекта, и расширяет выделенную на предыдущем этапе область
+    
+    # CHAIN_APPROX_SIMPLE
+    сontours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) # нахождение массива контурных точек
+    
+    
+    for contour in сontours:
+        (x, y, w, h) = cv2.boundingRect(contour) # преобразование массива из предыдущего этапа в кортеж из четырех координат
+    
+        # метод contourArea() по заданным contour точкам, здесь кортежу, вычисляет площадь зафиксированного объекта в каждый момент времени, это можно проверить
+        print(cv2.contourArea(contour))
+    
+        if cv2.contourArea(contour) < 700: # условие при котором площадь выделенного объекта меньше 700 px
+            continue
+
+        cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2) # получение прямоугольника из точек кортежа
+        cv2.putText(frame1, "Status: {}".format("Dvigenie"), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3, cv2.LINE_AA) # вставляем текст
+    
+    # cv2.drawContours(frame1, сontours, -1, (0, 255, 0), 2) # также можно было просто нарисовать контур объекта
+    
+    cv2.imshow("frame1", frame1)
+    frame1 = frame2  #
+    ret, frame2 = vidCap.read() #  
+ 
+    if cv2.waitKey(40) == 27:
+        break
+
+vidCap.release()
+cv2.destroyAllWindows()
