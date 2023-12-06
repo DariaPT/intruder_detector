98832e857f8474e339cbfcadba0408cb9855f946
VEGABIG\Rusll <rusllann97@gmail.com>
2023-12-06 21:56:14 +0500

    Добавлен тестовый функционал выделения рамки

--- intruder_detector.py
+++ intruder_detector.py
@@ -1,30 +1,82 @@
 import cv2
-import tkinter
+import tkinter as tk
 from PIL import Image, ImageTk
+import numpy as np
+from matplotlib import cm
 
 FILE_NAME = "video1.mp4"
-
 
 vidCap = cv2.VideoCapture(f'Video/{FILE_NAME}')
 
 vidCap.set(3,1280)
 vidCap.set(4,700)
 
-canvas = tkinter.Canvas(width=512, height=512, cursor="cross")
-canvas.pack(side="top", fill="both", expand=True)
-canvas.bind("<ButtonPress-1>", self.on_button_press)
-canvas.bind("<B1-Motion>", self.on_move_press)
-canvas.bind("<ButtonRelease-1>", self.on_button_release)
-
-rect = None
-
-start_x = None
-start_y = None
-
-_draw_image()
-
 ret, frame1 = vidCap.read()
 ret, frame2 = vidCap.read()
+##################
+
+class ExampleApp(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.x = self.y = 0
+        self.canvas = tk.Canvas(self, width=512, height=512, cursor="cross")
+        self.canvas.pack(side="top", fill="both", expand=True)
+        self.canvas.bind("<ButtonPress-1>", self.on_button_press)
+        self.canvas.bind("<B1-Motion>", self.on_move_press)
+        self.canvas.bind("<ButtonRelease-1>", self.on_button_release)
+
+        self.rect = None
+
+        self.start_x = None
+        self.start_y = None
+
+        self._draw_image()
+
+    def _draw_image(self):
+        img_cv2 = cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB)
+        self.im = Image.fromarray(img_cv2)
+        self.tk_im = ImageTk.PhotoImage(self.im)
+        self.canvas.create_image(0, 0, anchor="nw", image=self.tk_im)
+
+    def on_button_press(self, event):
+        # save mouse drag start position
+        self.start_x = event.x
+        self.start_y = event.y
+
+        #one rectangle
+        if not self.rect:
+            self.rect = self.canvas.create_rectangle(self.x, self.y, 1, 1, )
+
+    def on_move_press(self, event):
+        curX, curY = (event.x, event.y)
+
+        # expand rectangle as you drag the mouse
+        self.canvas.coords(self.rect, self.start_x, self.start_y, curX, curY)
+
+    def on_button_release(self, event):
+        global app
+
+        global xStart
+        global yStart
+        global xEnd
+        global yEnd
+
+        xStart = self.start_x
+        yStart = self.start_y
+        xEnd = event.x
+        yEnd = event.y
+        app.quit()
+
+xStart = 0
+yStart = 0
+xEnd = 0
+yEnd = 0
+
+app = ExampleApp()
+app.mainloop()
+
+
+########################
 
 while vidCap.isOpened():
     diff = cv2.absdiff(frame1, frame2)
